c3c55368bac3c969943fff350ceb5d15b512e61c
siehi <shmelevolo45@gmail.com>
2020-10-25 23:14:28 -0700

    Types controller + get all types endpoint implementation

---
+++ Heldy-API/Heldy-API/Controllers/TypesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Heldy.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Heldy_API.Controllers
+{
+    [ApiController]
+    [Route("types")]
+    public class TypesController : Controller
+    {
+        private ITypeService _typeService;
+
+        public TypesController(ITypeService typeService)
+        {
+            _typeService = typeService;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> GetTypes()
+        {
+            var types = await _typeService.GetTypesAsync();
+            return Ok(types);
+        }
+    }
+}
